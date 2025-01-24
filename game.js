1ace78bb53e7022f84c23f9c1973624ed1d0bdd5
Sahutsawat Sivakosit <aong1244@gmail.com>
2025-01-24 17:18:00 +0700

    Finish implementing minimum requirement of pokdeng game

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 const readlineSync = require('readline-sync');
 
 
-
 function shuffling(decks) {
     let currentIndex = decks.length, randomIndex;
     while (currentIndex != 0) {
@@ -12,17 +11,12 @@ function shuffling(decks) {
         // console.log(decks)
     }
     return decks;
-
 }
 
-
-// const a = shuffling ([1,2,3,4,5,6,7,8,9,10]);
-function playing(){
-    const bettingInput = readlineSync.question('Please put your bet\n');
+function playingPokdeng(bettingCoin){
     const suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
     const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     const decks = [];
-    const players = [];
     const createDeck = () => {
         for (let suit of suits) {
             for (let rank of ranks) {
@@ -34,8 +28,9 @@ function playing(){
     const shuffledDeck = shuffling(createDeck());
     const player1Cards = shuffledDeck.slice(0, 2);
     const player2Cards = shuffledDeck.slice(2, 4);
-    console.log(`You got ${player1Cards[0]['suits']}-${player1Cards[0]['ranks']}, ${player1Cards[1]['suits']}-${player1Cards[1]['ranks']}`)
-    console.log(`The dealer got ${player2Cards[0]['suits']}-${player2Cards[0]['ranks']}, ${player2Cards[1]['suits']}-${player2Cards[1]['ranks']}`)
+
+    console.log(`You got ${player1Cards[0]['suit']}-${player1Cards[0]['rank']}, ${player1Cards[1]['suit']}-${player1Cards[1]['rank']}`)
+    console.log(`The dealer got ${player2Cards[0]['suit']}-${player2Cards[0]['rank']}, ${player2Cards[1]['suit']}-${player2Cards[1]['rank']}`)
 
     const calculateCardValue = (cards) => {
         let sum = 0;
@@ -48,15 +43,39 @@ function playing(){
     }
     const player1CardsValue = calculateCardValue(player1Cards)
     const player2CardsValue = calculateCardValue(player2Cards) 
-    console.log()
     if (player1CardsValue > player2CardsValue) {
-        bettingCoin *= 2
-        console.log(`You won!!!, received ${bettingInput} chips`)
+        console.log(`You won!!!, received ${bettingCoin} chips`)
     }else if (player1CardsValue < player2CardsValue) {
+        bettingCoin = -bettingCoin
+        console.log(`You lose!!!, losed ${bettingCoin} chips`)
+    }else {
         bettingCoin = 0
-        console.log(`You lose!!!, losed ${bettingInput} chips`)
+        console.log(`You Tie!!!, received nothing`)
+
+    }
+    return bettingCoin
+}
+
+function main() {
+    let net_chip = 0
+    while (true) {
+        const bettingCoin = readlineSync.question('Please put your bet\n');
+        const money = playingPokdeng(bettingCoin);
+        net_chip += parseInt(money)
+        const playAgain = readlineSync.question('Wanna play more (Yes/No)?\n');
+        console.log()
+        if (playAgain === 'No') {
+            break
+        }
+    }
+    if (net_chip > 0) {
+        console.log(`You got total ${net_chip} chips`)
+    }else if (net_chip < 0) {
+        net_chip = Math.abs(net_chip)
+        console.log(`You lose total ${net_chip} chips`)
+    }else {
+        console.log(`You got nothing`)
     }
 
-
 }
-// playing();
+main()
